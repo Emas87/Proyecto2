0dea52ff663b7ec0eb10d5498102f9a7a2f0b9c7
oblandino <37023714+oblandino@users.noreply.github.com>
2018-04-20 22:36:11 -0600

    Add files via upload
    oblandino: Adding first version of distribution and argument check function

---
+++ dist.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <math.h>
+
+void initrand() {
+	srand((unsigned)time(NULL));
+}
+
+double dist(double media){
+
+  initrand();
+
+  double u = rand() / (RAND_MAX + 1.0);
+
+  // Formula = -ln(1-u)/lambda
+  // Media = 1/lambda
+  // Formula Final = -media * ln(1-u)
+  // Donde u es un valor entre 0 y 1 que indica la aleatoridad
+  double dist_exp = -media * log(1 - u); 
+  
+  printf("U: %f \nDistribucion: %f \n", u, dist_exp);
+
+  return dist_exp;
+
+}
+
